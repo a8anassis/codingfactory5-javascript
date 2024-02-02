3d755d2ad999ed8bbcaa542da7dba4070de64dcc
a8ana <a8anassis@gmail.com>
2024-02-02 19:33:13 +0200

    note app initial commit

---
+++ notes/notes.js
@@ -0,0 +1,42 @@
+let noteId = 0
+const daysGR = ['Κυριακή', 'Δευτέρα', 'Τρίτη', 'Τετάρτη', 'Πέμπτη', 'Παρασκευή', 'Σάββατο']
+const monthsGR = ['Ιανουαρίου', 'Φεβρουαρίου', 'Μαρτίου', 'Απριλίου', 'Μαΐου', 
+                    'Ιουνίου', 'Ιουλίου', 'Αυγούστου', 'Σεπτεμβρίου', 'Οκτωβρίου',
+                    'Νοεμβρίου', 'Δεκεμβρίου']
+
+window.addEventListener('DOMContentLoaded', function() {
+
+    this.setInterval(printGRDate, 1000)
+
+    this.document.querySelector('#addNoteBtn').addEventListener('click', function() {
+        insertNote(document.querySelector('#inputNote').value.trim())
+        reset()
+    })
+
+    this.document.querySelector('#inputNote').addEventListener('keyup', function(e) {
+        if (e.key === 'Enter') {
+            insertNote(this.value.trim())
+            reset()
+        }
+    })
+})
+
+function printGRDate() {
+    const currentDate = new Date()
+    const day = currentDate.getDay()
+    const date = currentDate.getDate()
+    const month = currentDate.getMonth()
+    const year = currentDate.getFullYear()
+    const hours = currentDate.getHours()
+    const minutes = currentDate.getMinutes()
+    const seconds = currentDate.getSeconds()
+
+    let dayStr = daysGR[day]
+    let monthStr = monthsGR[month]
+    let dateStr = `${dayStr}, ${date} ${monthStr} ${year}`
+    let timeStr = `${(hours < 10) ? '0' : ''}${hours}:
+                    ${(minutes < 10) ? '0' : ''}${minutes}:
+                    ${(seconds < 10) ? '0' : ''}${seconds}`
+                    
+    document.querySelector('#dateTxt').innerHTML = dateStr + "<br>" + timeStr
+}
