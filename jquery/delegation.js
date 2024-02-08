039a4f1c17a3be95bbcf69ec8eaa9b1d68ec231d
a8ana <a8anassis@gmail.com>
2024-02-08 18:59:15 +0200

    delegation updates

--- jquery/delegation.js
+++ jquery/delegation.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
-    $('#menu').on('click', 'li', function() {
-        console.log('Clicked on: ' + $(this).text())
-    })
+    insertLink()
+    // $('#menu').on('click', 'li', function() {
+    //     console.log('Clicked on: ' + $(this).text())
+    // })
 })
+
+// function insertLink() {
+//     $('#menu > li').each((index, el) => {
+//         if ($(el).is('#link')) {
+//             $(el).append(`<br><a href="https://codingfactory.aueb.gr" target="_blank">Visit us</a>`)
+//         }
+//     })
+// }
+
+const response = {title: "Coding Factory", link: "https://codingfactory.aueb.gr"}
+
+function insertLink() {
+    $('#menu').find('[id]').each(function(index, el) {
+        if ($(el).is('a')) {
+            $(el).attr('href', response[el.id])
+        }
+    })
+}
