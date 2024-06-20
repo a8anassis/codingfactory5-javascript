1759a103c3a0d0111171509bd2636316a3ee81e2
Th. Androutsos <a8anassis@gmail.com>
2024-06-20 17:22:03 +0300

    updates

--- jquery/delegation.js
+++ jquery/delegation.js
@@ -1,24 +1,24 @@
-$(document).ready(function() {
+$(function() {
     insertLink()
     // $('#menu').on('click', 'li', function() {
     //     console.log('Clicked on: ' + $(this).text())
     // })
 })
 
-// function insertLink() {
-//     $('#menu > li').each((index, el) => {
-//         if ($(el).is('#link')) {
-//             $(el).append(`<br><a href="https://codingfactory.aueb.gr" target="_blank">Visit us</a>`)
-//         }
-//     })
-// }
-
-const response = {title: "Coding Factory", link: "https://codingfactory.aueb.gr"}
-
 function insertLink() {
-    $('#menu').find('[id]').each(function(index, el) {
-        if ($(el).is('a')) {
-            $(el).attr('href', response[el.id])
+    $('#menu > li').each((index, el) => {
+        if ($(el).is('#link')) {
+            $(el).append(`<br><a href="https://codingfactory.aueb.gr" target="_blank">Visit us</a>`)
         }
     })
 }
+
+// const response = {title: "Coding Factory", link: "https://codingfactory.aueb.gr"}
+
+// function insertLink() {
+//     $('#menu').find('[id]').each(function(index, el) {
+//         if ($(el).is('a')) {
+//             $(el).attr('href', response[el.id])
+//         }
+//     })
+// }
